130d17df5810b83fe7020da3a9ed3214b9cbbae0
sailendrachettri <sailendra9083@gmail.com>
2024-02-06 13:24:10 +0530

    height and width fixed for projects

--- js/commonScript.js
+++ js/commonScript.js
@@ -1,7 +1,16 @@
+// --------------TRUNCATE TITLE AND DESCRIPTION OF THE PROJECT--------------
+
 let projectDesc = document.querySelectorAll('.project-description');
+let projectTitle = document.querySelectorAll('.project-title');
 
 projectDesc.forEach(ele =>{
-    // console.log(ele.innerText.length);
+    console.log(ele.innerText.length);
     if(ele.innerText.length > 150)
         ele.innerText = ele.innerText.substring(0, 150) + "...";
+})
+
+projectTitle.forEach(ele =>{
+    console.log(ele.innerText.length);
+    if(ele.innerText.length > 40)
+        ele.innerText = ele.innerText.substring(0, 150) + "..";
 })
