86a3f4737b9db8b58337fb8c7f5a99c18aca4d29
sailendrachettri <sailendra9083@gmail.com>
2024-02-09 16:02:46 +0530

    last project updated

--- js/commonScript.js
+++ js/commonScript.js
@@ -1,3 +1,29 @@
+// SETTING THE DATE AND TIME FOR PROJECT UPDATION
+let projectGkSite = document.getElementById("project-gk-site");
+projectGkSite.innerText = "Dec 2023";
+let projectTextAnalyzer = document.getElementById("project-text-analyzer");
+projectTextAnalyzer.innerText = "June 2023";
+let projectChecklist = document.getElementById("project-checklist");
+projectChecklist.innerText = "Jan 2024";
+let projectNBBGCForcasting = document.getElementById("project-nbbgc-forcasting");
+projectNBBGCForcasting.innerText = "March 2023";
+let projectLaugnDude = document.getElementById("project-laugh-dude");
+projectLaugnDude.innerText = "Dec 2022";
+let projectEncryasmi = document.getElementById("project-encryasmi");
+projectEncryasmi.innerText = "Sept 2022";
+let projectAlphanews = document.getElementById("project-alphanews");
+projectAlphanews.innerText = "Oct 2023";
+let projectWinOverCrush = document.getElementById("project-win-over-crush");
+projectWinOverCrush.innerText = "Feb 2024";
+
+
+
+
+
+
+
+
+
 // --------------TRUNCATE TITLE AND DESCRIPTION OF THE PROJECT--------------
 
 let projectDesc = document.querySelectorAll('.project-description');
