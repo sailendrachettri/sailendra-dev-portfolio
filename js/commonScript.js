dfd2248b00b56c0665a5a7f23d0df1341c892d97
sailendrachettri <sailendra9083@gmail.com>
2024-02-12 16:09:14 +0530

    project btn color changed

--- js/commonScript.js
+++ js/commonScript.js
@@ -18,25 +18,17 @@ projectWinOverCrush.innerText = "Feb 2024";
 
 
 
-
-
-
-
-
-
 // --------------TRUNCATE TITLE AND DESCRIPTION OF THE PROJECT--------------
 
 let projectDesc = document.querySelectorAll('.project-description');
 let projectTitle = document.querySelectorAll('.project-title');
 
 projectDesc.forEach(ele =>{
-    console.log(ele.innerText.length);
     if(ele.innerText.length > 150)
         ele.innerText = ele.innerText.substring(0, 150) + "...";
 })
 
 projectTitle.forEach(ele =>{
-    console.log(ele.innerText.length);
     if(ele.innerText.length > 40)
         ele.innerText = ele.innerText.substring(0, 150) + "..";
 })
